aee6a2cd2447433dbf45df21dddd4cfd0fd2d86c
Leonardo-Santos <leonardo.co.de23@gmail.com>
2023-08-09 21:12:42 -0300

    components

--- react_vsconnect-main/src/main.tsx
+++ react_vsconnect-main/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 // import App from './App'
 import './index.css'
 import Home from './pages/Home'
-import ListaSevicos from './pages/servicos'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    < ListaSevicos />
+    <Header/>
+    < Home />
+    <Footer/>
   </React.StrictMode>
 )
