53b5f22fa1ce5fbf06fb1d24746cf51af6b4eced
Leonardo-Santos <leonardo.co.de23@gmail.com>
2023-08-11 21:51:29 -0300

    Aula 11-08

--- react_vsconnect-main/src/main.tsx
+++ react_vsconnect-main/src/main.tsx
@@ -4,12 +4,13 @@ import { Route, Routes, BrowserRouter } from "react-router-dom"
 // import App from './App'
 import './index.css'
 import Home from './pages/Home'
-import ListaServicos from './pages/servicos'
+import ListaServicos from './pages/Servicos'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import CadastroDev from './pages/CadastroDev'
 import CadastroServ from './pages/CadastroServ'
 import Login from './pages/Login'
+import ListaDevs from './pages/ListaDevs'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='teste2' element={ < CadastroDev />} />
         <Route path='teste3' element={ < CadastroServ />} />
         <Route path='teste4' element={ < Login />} />
+        <Route path='teste5' element={ < ListaDevs />} />
       </Routes>
       <Footer />
       
