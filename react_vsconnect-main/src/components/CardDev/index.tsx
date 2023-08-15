62a364ad67abff3852cb80c50757531f11d120e2
leonardo da silva <leonardo.co.de23@gmail.com>
2023-08-15 14:10:54 -0300

    adicionei a função barra de pesquisa

--- react_vsconnect-main/src/components/CardDev/index.tsx
+++ react_vsconnect-main/src/components/CardDev/index.tsx
@@ -1,22 +1,23 @@
 import "./style.css"
+
 export default function CardDev(props: any) {
 
     return (
-        <>
-            <div className="dev">
-                <div className="grupo_contato">
-                    <img src="https://github.com/JessicaSanto.png" alt="" />
-                    <div className="contato_dev">
-                        <h3>{ props.nome }</h3>
-                        <p>{ props.email }</p>
-                    </div>
-                </div>
-                <div className="techs">
-                    <span>HTML</span>
-                    <span>CSS</span>
-                    <span>React</span>
+        <div className="dev">
+            <div className="grupo_contato">
+                <img src={props.foto} alt="" />
+                <div className="contato_dev">
+                    <h3>{ props.nome }</h3>
+                    <p>{ props.email }</p>
                 </div>
             </div>
-        </>
+            <div className="techs">
+            {
+                props.techs.map((tech: string, index: number) => {
+                    return <span key={index}>{tech}</span>
+                })
+            }
+            </div>
+        </div>
     )
 }
